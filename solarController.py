0dfeb833bff9b2f6781fbc366b60736bb3a25e49
Pratscher, Martin <martin.pratscher@anton-paar.com>
2025-07-30 10:25:49 +0200

    Refactor code

--- solarController.py
+++ solarController.py
@@ -11,8 +11,8 @@ def getStatusState(status):
 def getPitchState(pitch):
     return getQuantityState(pitch)
 
-def isSolarControllerConnected(solar_controller_status):
-    if solar_controller_status == 'on':
+def isSolarControllerConnected(status):
+    if status == 'on':
         return True
     else:
         return False
@@ -44,7 +44,26 @@ def setUpDownSpeed(speed):
     service_data = {"entity_id": "light.esp32_solar_control_1_axis_speed_up_down", "brightness": speed}
     hass.services.call("light", "turn_on", service_data, False)
 
+class SolarController:
+    def __init__(self, name, status, pitch):
+        self.name = name
+        self.status = status
+        self.pitch = pitch
+
+def getSolarControllerFromInput():
+    name = data.get("solar_controller_name") # "1-axis" or "2-axis"
+
+    if name == ONE_AXIS_ID or name == TWO_AXIS_ID:
+        logger.debug(f"Solar controller name is {name}")
+        status = "binary_sensor." + DEVICE_NAME_PREFIX + name + "_status"
+        pitch = "sensor." + DEVICE_NAME_PREFIX + name + "_mpu6050_pitch"
+        return SolarController(name, status, pitch)
+    else:
+        logger.error(f"Solar controller {name} is not valid")
+        return None
+
 TIMEOUT = 10 # s
+DEVICE_NAME_PREFIX = "esp32_solar_control_"
 
 ONE_AXIS_ID = "1-axis"
 TWO_AXIS_ID = "2-axis"
@@ -52,27 +71,14 @@ TWO_AXIS_ID = "2-axis"
 PITCH_HYSTERESIS_UPPER = 3.0
 PITCH_HYSTERESIS_LOWER = 0.5
 
-solar_controller_name = data.get("solar_controller_name") # "1-axis" or "2-axis"
-solar_controller_status = None
-solar_controller_pitch = None
+sc = getSolarControllerFromInput()
 
-if solar_controller_name == ONE_AXIS_ID:
-    logger.debug(f"Solar controller name is {solar_controller_name}")
-    solar_controller_status = "binary_sensor.esp32_solar_control_1_axis_status"
-    solar_controller_pitch = "sensor.esp32_solar_control_1_axis_mpu6050_pitch"
-elif solar_controller_name == TWO_AXIS_ID:
-    logger.debug(f"Solar controller name is {solar_controller_name}")
-    solar_controller_status = "binary_sensor.esp32_solar_control_2_axis_status"
-    solar_controller_pitch = "sensor.esp32_solar_control_2_axis_mpu6050_pitch"
-else:
-    logger.error(f"Solar controller name is not valid")
-
-solar_controller_status_state = getStatusState(solar_controller_status)
+solar_controller_status_state = getStatusState(sc.status)
 solar_controller_pitch_state = None
 
 if isSolarControllerConnected(solar_controller_status_state):
-    solar_controller_pitch_state = getPitchState(solar_controller_pitch)
-    logger.warning(f"Solar controller {solar_controller_pitch} is: {solar_controller_pitch_state}")
+    solar_controller_pitch_state = getPitchState(sc.pitch)
+    logger.warning(f"Solar controller {sc.pitch} is: {solar_controller_pitch_state}")
     
     pitch = float(solar_controller_pitch_state)
     
@@ -81,7 +87,7 @@ if isSolarControllerConnected(solar_controller_status_state):
         switchOnUp()
         setUpDownSpeed(50)
         while (pitch > 0.0 and timeout > 0):
-            pitch = float(getPitchState(solar_controller_pitch))
+            pitch = float(getPitchState(sc.pitch))
             time.sleep(1)
             timeout = timeout - 1
             logger.warning("Positive pitch now negative")
@@ -92,7 +98,7 @@ if isSolarControllerConnected(solar_controller_status_state):
         switchOnDown()
         setUpDownSpeed(50)
         while (pitch < 0.0 and timeout > 0):
-            pitch = float(getPitchState(solar_controller_pitch))
+            pitch = float(getPitchState(sc.pitch))
             time.sleep(1)
             timeout = timeout - 1
             logger.warning("Negative pitch now positive")
