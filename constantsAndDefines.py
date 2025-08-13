7ecd653546706b8af0b8d46b886981ecd9c1bb6d
Martin Pratscher <martin.pratscher@gmx.net>
2025-08-13 20:35:25 +0200

    Introduce constants and defines file

---
+++ constantsAndDefines.py
@@ -0,0 +1,9 @@
+from enum import IntEnum
+
+class UpDownPosition(IntEnum):
+    MinimizeDifference = 0
+    Protect            = 1
+    
+class EastWestPosition(IntEnum):
+    MinimizeDifference = 0
+    Protect            = 1
